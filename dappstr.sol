335589591b4cf7916cccfa9ad69dca85e236e325
Evert0x <evert@hatching.io>
2018-10-27 21:48:13 +0200

    update smart contract

--- dappstr.sol
+++ dappstr.sol
@@ -1,16 +1,16 @@
 pragma solidity ^0.4.24;
 
 contract DappStore{
-    event Sign(address app, string name, string category, string homepage, string icon);
-    event StageUpdate(address app, string stage);
+    event Sign(address app, string name, string category, string homepage, string icon, string blockchain);
+    event StageUpdate(address app, string name, string stage);
     //string[] states = ["protoype", "mvp", "alpha", "beta", "live"];
 
-    function SignUp(string name, string category, string homepage, string icon) public {
-        emit Sign(msg.sender, name, category, homepage, icon);
-        emit StageUpdate(msg.sender, "prototype");
+    function SignUp(string name, string category, string homepage, string icon, string blockchain) public {
+        emit Sign(msg.sender, name, category, homepage, icon, blockchain);
+        emit StageUpdate(msg.sender, name, "prototype");
     }
 
-    function Update(string stage) public {
-        emit StageUpdate(msg.sender, stage);
+    function Update(string name, string stage) public {
+        emit StageUpdate(msg.sender, name, stage);
     }
 }
